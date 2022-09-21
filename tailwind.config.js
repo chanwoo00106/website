2603eedc4560cb73c2c6659ed23e12c5f54b3aa1
chanwoo00106 <chan00106@naver.com>
2022-09-21 22:26:06 +0900

    Add tailwind setting

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  themes: {
+    letterSpacing: {
+      tight: "-.05em",
+    },
+    extends: {
+      height: {
+        "half-screen": "50vh",
+      },
+    },
+  },
+  plugins: [],
+};
