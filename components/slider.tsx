17322be68f7919cc4f0522ee83e26e8cd2a1c561
chanwoo00106 <chan00106@naver.com>
2022-09-29 23:42:38 +0900

    Add use-animation-frame

--- components/slider.tsx
+++ components/slider.tsx
@@ -1,5 +1,6 @@
-import React, { useContext, useRef } from "react";
+import React, { useCallback, useContext, useRef } from "react";
 import { SizeContext } from "./size-observer";
+import useAnimationFrame from "./use-animation-frame";
 
 interface Props {
   children?: React.ReactNode;
@@ -20,16 +21,49 @@ const SliderContainer: React.FC<Props> = ({
   const refContent = useRef<HTMLDivElement>(null);
 
   const enabled = innerWidth < contentWidth;
+
+  useAnimationFrame(
+    enabled,
+    useCallback(() => {
+      const { current: elContainer } = refContainer;
+      const { current: elContent } = refContent;
+
+      if (elContainer && elContent) {
+        refScrollX.current += 0.5;
+        elContainer.scrollLeft = refScrollX.current;
+        if (elContainer.scrollLeft >= elContent.clientWidth) {
+          refScrollX.current = 0;
+          elContainer.scrollLeft = 0;
+        }
+      }
+    }, [])
+  );
+
   return (
     <div
-      ref={refContent}
+      ref={refContainer}
       className={`slider-container overflow-x-hidden whitespace-nowrap max-w-full pointer-events-none ${className}`}
     >
       <div ref={refContent} className="inline-block">
         {children}
       </div>
+      <div className={enabled ? "inline-block" : "hidden"}>{children}</div>
     </div>
   );
 };
 
 export default SliderContainer;
+
+interface ItemProps {
+  children?: React.ReactNode;
+  width: number;
+}
+
+export const SliderItem: React.FC<ItemProps> = ({ children, width }) => (
+  <div
+    className="inline-flex justify-center items-center mx-4"
+    style={{ width }}
+  >
+    {children}
+  </div>
+);
